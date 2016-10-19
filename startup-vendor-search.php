26ab7f1591bf2c31efe43b8a70369b85f41867f8
yozz <yozz.online@gmail.com>
2016-10-19 16:37:42 -0400

    Initial commit

---
+++ startup-vendor-search.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: StartUp Vendor Search
+Author: Yann Caplain
+Version: 1.0.0
+Text Domain: startup-vendor-search
+Domain Path: /languages
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Shortcode to put all vendors in a list
+function startup_vendor_search_shortcode( $atts ) {
+    wp_enqueue_script( 'startup-vendor-search-select2', plugins_url() . '/startup-vendor-search/lib/select2/select2.min.js', array( ), '', false );
+    wp_enqueue_style( 'startup-vendor-search-select2', plugins_url() . '/startup-vendor-search/lib/select2/select2.min.css' );
+    $vendors = '';
+    $terms = get_terms( 'wcpv_product_vendors' );
+    foreach ( $terms as $term ) {
+        $term_link = get_term_link( $term, 'wcpv_product_vendors' );
+        if ( is_wp_error( $term_link ) )
+            continue;
+            $vendors .= '<option value="' . $term_link . '">' . $term->name . '</option>';
+    }
+    return '<select id="product_vendor_list" class="select2" style="width: 100%">
+    <option value="">Recherche par nom</option>
+    ' . $vendors . '
+    </select>
+    <script>
+        jQuery(document).ready(function() {
+          jQuery("#product_vendor_list").select2({
+              placeholder: "Recherche par nom",
+              allowClear: false
+            });
+        });
+
+        // On fonce sur la page à la selection
+        jQuery(function(){
+            // bind change event to select
+            jQuery("#product_vendor_list").on("change", function () {
+                var url = jQuery(this).val(); // get selected value
+                if (url) { // require a URL
+                    window.location = url; // redirect
+                }
+                return false;
+            });
+        });
+    </script>';
+}
+
+add_shortcode( 'list_vendors', 'startup_vendor_search_shortcode' );
